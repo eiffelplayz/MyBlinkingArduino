7ca1a3bb616bf78fb9db254d8980e27e8fe993ce
eiffelplayz <eiffelplayz@gmail.com>
2020-06-29 16:34:50 +0800

    set to pin 8

--- app.ino
+++ app.ino
@@ -1,15 +1,12 @@
 // Prepare LED initialization for the fist time
 void setup() {
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
+  pinMode(8, OUTPUT);
 }
 
 // Keep LED on and off in 1000 milliseconds ( 1 second )
 void loop() {
-   digitalWrite(10, HIGH);
-   digitalWrite(11, HIGH);
+   digitalWrite(8, HIGH);
    delay(1000);
-   digitalWrite(10, LOW);
-   digitalWrite(11, LOW); 
+   digitalWrite(8, LOW);
    delay(1000);
 }
